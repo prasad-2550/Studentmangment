769952bc4acb73f52963461a087af49467ad6751
prasad-2550 <pawar.prasad5025@gmail.com>
2023-04-18 14:27:35 +0530

    first commmit by prasad

---
+++ src/main/java/com/BikkadIT/AdditionLogic/Calculator/Sum.java
@@ -0,0 +1,12 @@
+package com.BikkadIT.AdditionLogic.Calculator;
+
+public class Sum {
+	
+	public int sum (int a, int b)
+	{
+		 int c= a+b;
+		 System.out.println("the addition is:" +c);
+		 return c;
+	}
+
+}
